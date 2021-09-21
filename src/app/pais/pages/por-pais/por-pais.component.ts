6fc6d2cecb1e0e0990aa61c789f064b4b139c04e
eduardocc97 <73180505+eduardocc97@users.noreply.github.com>
2021-09-21 10:26:45 -0500

    Se hicieron funcional los elementos del sidebar y ya se recibe que se escribe en el campo de busqueda

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -8,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PorPaisComponent implements OnInit {
 
+  termino:string = ""
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  buscar() {
+    console.log(this.termino);
+  }
 }
